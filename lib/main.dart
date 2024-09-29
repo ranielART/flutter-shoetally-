fdf7f29e759433284d3862f37bcd5133e7bfe877
Mc Bernard Logronio <126197713+dreamymc@users.noreply.github.com>
2024-09-29 19:32:02 +0800

    Mock dashboard (#15)
    * database models (#3)
    
    * Model thingy (#2)
    
    * for testing branch
    
    * filesys update (#1)
    
    * init model
    
    ---------
    
    Co-authored-by: jl <orionjl1106@gmail.com>
    
    * modified models
    
    * Update README.md
    
    ---------
    
    Co-authored-by: sef-UM <j.orion.536041@umindanao.edu.ph>
    
    * added comment
    
    * login-page polishing finished
    
    * fixed on-screen keyboard issue
    
    * sign-up page completed
    
    * sampleCommit
    
    * finished polishing the login page
    
    * init resolution-experiment
    
    * fixed on-screen keyboard issue on login and signup
    
    * added dashboard
    
    ---------
    
    Co-authored-by: jlorion <86958537+jlorion@users.noreply.github.com>
    Co-authored-by: sef-UM <j.orion.536041@umindanao.edu.ph>
    Co-authored-by: Raniel Art Montebon <122724339+ranielART@users.noreply.github.com>
    Co-authored-by: ranielART <ranielmontebon24@gmail.com>
    Co-authored-by: Takezo <j.reyes.536490@umindanao.edu.ph>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:commerce_mobile/screens/dashboard.dart';
 import 'package:commerce_mobile/screens/login_screen.dart';
 import 'package:commerce_mobile/screens/signup_screen.dart';
 import 'package:commerce_mobile/screens/splash_screen.dart';
@@ -14,6 +15,7 @@ class ShoeTallyApp extends StatelessWidget {
         routes: {
           '/login': (context) => const LoginScreen(),
           '/signup': (context) => const SignupScreen(),
+          '/dashboard': (context) => Dashboard(),
         });
   }
 }
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  final List<Map<String, String>> _transactions = [
+    {
+      'title': 'Century Tuna Ila Bernard',
+      'price': '₱ 25,000.00',
+      'dateTime': 'September 29, 2024, 5:00 PM'
+    },
+    {
+      'title': 'Century Tuna Ila Bernard',
+      'price': '₱ 25,000.00',
+      'dateTime': 'September 29, 2024, 5:00 PM'
+    },
+    {
+      'title': 'Kyle Sex',
+      'price': '₱ 25,000.00',
+      'dateTime': 'September 29, 2024, 5:00 PM'
+    },
+    {
+      'title': 'Century Tuna Ila Bernard',
+      'price': '₱ 25,000.00',
+      'dateTime': 'September 29, 2024, 5:00 PM'
+    },
+    {
+      'title': 'Century Tuna Ila Bernard',
+      'price': '₱ 25,000.00',
+      'dateTime': 'September 29, 2024, 5:00 PM'
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFA259FF),
+        foregroundColor: Colors.white,
+        title: Text(
+          "Dashboard",
+          style: GoogleFonts.inter(
+            fontSize: 24,
+            letterSpacing: -0.5,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            // Open drawer
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 35),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Product and Customer Info
+            Center(
+              child: Column(
+                children: [
+                  _infoCard("Total Products", "189893"),
+                  const SizedBox(height: 16),
+                  _infoCard("Number of Customers", "25"),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Profit Info
+            _profitCard("Profit", "PHP 100,000"),
+            const SizedBox(height: 35),
+            // Transactions Title
+            Text(
+              "Transactions",
+              style: GoogleFonts.inter(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xFFA259FF),
+              ),
+            ),
+            const SizedBox(height: 8),
+            // Transactions List
+            Expanded(
+              child: ListView(
+                children: [
+                  for (var i = 0; i < 3 && i < _transactions.length; i++)
+                    _transactionItem(
+                      _transactions[i]['title']!,
+                      _transactions[i]['price']!,
+                      _transactions[i]['dateTime']!,
+                    ),
+                ],
+              ),
+            ),
+            // View All Transactions Button
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "View All Transactions",
+                  style: GoogleFonts.inter(
+                    color: const Color(0xFFA259FF),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      // Bottom Navigation Bar
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: const Color(0xFFA259FF),
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+        ],
+      ),
+    );
+  }
+
+  Widget _infoCard(String title, String value) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 16.0),
+      decoration: BoxDecoration(
+        color: const Color(0xFFA259FF), // Purple background color
+        borderRadius: BorderRadius.circular(7.0), // Rounded corners
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: GoogleFonts.inter(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const SizedBox(height: 10),
+              Text(
+                value,
+                style: GoogleFonts.inter(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _profitCard(String title, String value) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey.shade300),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.inter(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xFFA259FF),
+            ),
+          ),
+          Text(
+            value,
+            style: GoogleFonts.inter(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xFFA259FF),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _transactionItem(String title, String price, String dateTime) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            title,
+            style: GoogleFonts.inter(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(dateTime, style: GoogleFonts.inter(fontSize: 14)),
+          trailing: Text(
+            "$price / Unit",
+            style: GoogleFonts.inter(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xFFA259FF),
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey.shade300,
+          thickness: 1,
+        ),
+      ],
+    );
+  }
+}
