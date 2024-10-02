745239550ba8de6a89f287302eb51c6da5d2a8e0
Mc Bernard Logronio <126197713+dreamymc@users.noreply.github.com>
2024-10-02 13:43:38 +0800

    app drawer (#20)
    * added dashboard
    
    * added AppDrawer component
    
    ---------
    
    Co-authored-by: sef-UM <j.orion.536041@umindanao.edu.ph>

---
+++ lib/compontents/app_drawer.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Color(0xFFA259FF),
+            ),
+            margin: EdgeInsets.zero,
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 25, // adjust size sa avatar
+                      backgroundImage: NetworkImage(
+                        'https://via.placeholder.com/150',
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'John Doe', // user name
+                          style: GoogleFonts.inter(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'john.doe@example.com', // user email or other info
+                          style: GoogleFonts.inter(
+                            fontSize: 14,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.dashboard),
+            title: const Text('Dashboard'),
+            onTap: () {
+              Navigator.pushNamed(context, '/dashboard');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.list_alt),
+            title: const Text('Products'),
+            onTap: () {
+              // products icon tap
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.receipt),
+            title: const Text('Orders'),
+            onTap: () {
+              // orders icon tap
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.history),
+            title: const Text('Transaction History'),
+            onTap: () {
+              // transaction history icon tap
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.people),
+            title: const Text('Customer List'),
+            onTap: () {
+              // customer list tap
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
