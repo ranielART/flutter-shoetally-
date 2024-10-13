f222fe2c1d9379efe53b12bf5929017474829fa0
ranielART <ranielmontebon24@gmail.com>
2024-10-14 03:59:06 +0800

    User features polished Added loading screens The current user now shows on the app drawer polished some of the buttons in the profile view

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,130 +1,111 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:commerce_mobile/controllers/userController.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:commerce_mobile/services/authentication/authentication.dart';
 import 'package:commerce_mobile/components/app_drawer.dart';
 import 'package:commerce_mobile/components/appbar.dart';
 import 'package:commerce_mobile/components/circle_avatar_component.dart';
 import 'package:commerce_mobile/components/navbar.dart';
-import 'package:commerce_mobile/models/UserProfile.dart';
-import 'package:commerce_mobile/services/authentication/auth_functions.dart';
-import 'package:commerce_mobile/services/authentication/authentication.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
-class UserProfilePage extends StatefulWidget {
-  @override
-  State<UserProfilePage> createState() => _UserProfilePageState();
-}
+class UserProfilePage extends StatelessWidget {
+  UserController userController = UserController();
 
-class _UserProfilePageState extends State<UserProfilePage> {
-  User? user;
-  Userprofile? userProfile;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUserData();
-  }
-
-  Future<void> _loadUserData() async {
-
-    //
-    user = await AuthFunctions().getCurrentUser();
-    if (user != null) {
-      final profile = await AuthenticationService()
-          .getUserProfile(user); // Await and pass the user instance
-      setState(() {
-        userProfile = profile;
-      }); // Trigger UI update
-    }
-  }
-  
+  UserProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(title: "User Profile"), // Custom app bar
-      drawer: const AppDrawer(), // Custom drawer
+      appBar: const CustomAppBar(title: "User Profile"),
+      drawer: const AppDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 30),
-            Center(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    const CircleAvatarComponent(), // Avatar with photo
-                    const SizedBox(height: 16),
-                    Text(
-                      userProfile!.name,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+        child: FutureBuilder<DocumentSnapshot>(
+          future: userController.getUserData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Error: ${snapshot.error}"));
+            } else if (snapshot.hasData && snapshot.data!.exists) {
+              var userData = snapshot.data!.data() as Map<String, dynamic>;
+              String name = userData['name'] ?? 'No name';
+              String email = userData['email'] ?? 'No email';
+
+              return Center(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
                       ),
-                    ),
-                    Text(
-                      user!.email.toString(),
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.settings),
-                      title: Text("Edit Profile"),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/edit-profile');
-                      },
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.settings),
-                      title: Text("Verify Email"),
-                      onTap: () async {
-                        User? user =
-                            await AuthenticationService().getCurrentUser();
-                        await user!.sendEmailVerification();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Verification Sent')),
-                        );
-                      },
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.logout),
-                      title: Text("Log Out"),
-                      onTap: () async {
-                        
-                        await AuthFunctions().signOut();
-                        User? user = await AuthFunctions().getCurrentUser();
-                        
-                        if (user == null) {
-                          setState(() {
-                            
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                              '/login',
-                              (route) => false,
+                    ],
+                  ),
+                  child: IntrinsicHeight(
+                    // Dynamically adjusts the height to fit content
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, // Prevent stretching
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const CircleAvatarComponent(),
+                        const SizedBox(height: 16),
+                        Text(
+                          name.toUpperCase(),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          email,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Divider(),
+                        ListTile(
+                          leading: Icon(Icons.settings_outlined),
+                          title: Text("Edit Profile"),
+                          onTap: () {
+                            Navigator.pushNamed(context, '/edit-profile');
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.email_outlined),
+                          title: Text("Verify Email"),
+                          onTap: () async {
+                            User? user =
+                                await AuthenticationService().getCurrentUser();
+                            await user!.sendEmailVerification();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Verification Sent')),
                             );
-                          });
-                        }
-                      },
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.logout),
+                          title: Text("Log Out"),
+                          onTap: () {
+                            FirebaseAuth.instance.signOut();
+                            Navigator.pushNamed(context, '/login');
+                          },
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ],
+              );
+            } else {
+              return Center(child: Text("User data not found"));
+            }
+          },
         ),
       ),
       bottomNavigationBar: const CustomBottomNavigationBar(
